fe50425fe8a203987ada1c91420e33c2749ff04b
blazer913 <blazeyah557@gmail.com>
2026-09-17 23:53:03 +0100

    Update WindowOverlay.hpp

--- src/WindowOverlay.hpp
+++ src/WindowOverlay.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include "WindowAnalyzer.hpp"
 #include <Geode/Geode.hpp>
+#include <functional>
+#include <string>
 
 namespace twa {
 
@@ -10,15 +12,20 @@ public:
 
     bool init() override;
     void setWindows(std::vector<InputWindow> windows);
+    void setStatus(std::string const& text);
     void setVisibleForUser(bool visible);
     bool visibleForUser() const { return m_userVisible; }
+
+    void setOnAnalyzeClicked(std::function<void()> cb) { m_onAnalyzeClicked = std::move(cb); }
 
 private:
     bool ccTouchBegan(cocos2d::CCTouch* touch, cocos2d::CCEvent* event) override;
     void ccTouchMoved(cocos2d::CCTouch* touch, cocos2d::CCEvent* event) override;
     void ccTouchEnded(cocos2d::CCTouch* touch, cocos2d::CCEvent* event) override;
 
+    void onAnalyzeButton(cocos2d::CCObject*);
     void rebuild();
+    void relayout();
     void savePosition();
     void loadPosition();
 
@@ -26,6 +33,9 @@ private:
     bool m_dragging = false;
     bool m_userVisible = true;
     cocos2d::CCLabelBMFont* m_text = nullptr;
+    cocos2d::CCLabelBMFont* m_status = nullptr;
+    cocos2d::CCMenu* m_menu = nullptr;
+    std::function<void()> m_onAnalyzeClicked;
 };
 
 }
