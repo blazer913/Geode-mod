79102ac8c75b2c4f36953d342483f6784ba301ba
blazer913 <blazeyah557@gmail.com>
2026-09-18 18:06:43 +0100

    Update State.hpp

--- src/State.hpp
+++ src/State.hpp
@@ -1,10 +1,16 @@
 #pragma once
 #include "Macro.hpp"
 #include "Controller.hpp"
+#include <vector>
 
 namespace cbot {
-
 class CbotOverlay;
+
+struct CheckpointData {
+    double time = 0.0;
+    size_t macroSize = 0;
+    size_t playIndex = 0;
+};
 
 class State {
 public:
@@ -13,13 +19,16 @@ public:
     bool recording = false;
     bool playing = false;
     bool overlayOn = false;
-    int frame = 0;
+    
+    double time = 0.0;
     size_t playIndex = 0;
+    bool needsReset = false; // Defers level reset until the game is unpaused
+
     CbotOverlay* hud = nullptr;
+    std::vector<CheckpointData> checkpoints;
 
     void refreshHud();
 };
 
 State& getState();
-
 }
