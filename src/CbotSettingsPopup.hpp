36925132d95b254ef3944d6480cc21337fe63e03
blazer913 <blazeyah557@gmail.com>
2026-09-18 12:34:50 +0100

    Create CbotSettingsPopup.hpp

---
+++ src/CbotSettingsPopup.hpp
@@ -0,0 +1,42 @@
+#pragma once
+#include <Geode/Geode.hpp>
+#include <Geode/ui/Popup.hpp>
+#include "Controller.hpp"
+#include <functional>
+
+namespace cbot {
+
+class CbotSettingsPopup : public geode::Popup {
+protected:
+    bool init();
+
+public:
+    static CbotSettingsPopup* create();
+    void setOnCommand(std::function<void(Command)> cb) { m_onCommand = std::move(cb); }
+
+private:
+    void showTab(int index);
+    void refreshLabels();
+
+    void onTabMacros(cocos2d::CCObject*);
+    void onTabFrameCounter(cocos2d::CCObject*);
+    void onRecord(cocos2d::CCObject*);
+    void onPlay(cocos2d::CCObject*);
+    void onToggleCounter(cocos2d::CCObject*);
+    void onCalculate(cocos2d::CCObject*);
+
+    cocos2d::CCNode* m_macrosPanel = nullptr;
+    cocos2d::CCNode* m_frameCounterPanel = nullptr;
+
+    cocos2d::CCLabelBMFont* m_macroStatusLabel = nullptr;
+    cocos2d::CCLabelBMFont* m_macroLengthLabel = nullptr;
+    cocos2d::CCMenuItemSpriteExtra* m_recordBtn = nullptr;
+    cocos2d::CCMenuItemSpriteExtra* m_playBtn = nullptr;
+
+    cocos2d::CCLabelBMFont* m_counterStateLabel = nullptr;
+    cocos2d::CCLabelBMFont* m_calculateResultLabel = nullptr;
+
+    std::function<void(Command)> m_onCommand;
+};
+
+}
