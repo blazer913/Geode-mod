e0eeddb02331b65c8b21b2fe3509f8930550b86c
blazer913 <blazeyah557@gmail.com>
2026-09-18 21:25:49 +0100

    Update MacroModMenu.hpp

--- src/MacroModMenu.hpp
+++ src/MacroModMenu.hpp
@@ -1,16 +1,33 @@
 #pragma once
 #include <Geode/Geode.hpp>
-#include <Geode/ui/Popup.hpp> // Required for Geode v2 Popups
 #include "macro.hpp"
 
 using namespace geode::prelude;
 
-class MacroModMenu : public geode::Popup<> {
+// Inherit directly from the game's native alert layer
+class MacroModMenu : public FLAlertLayer {
 protected:
-    bool setup() override {
-        this->setTitle("Macro Settings");
+    bool init() {
+        // 75 is the standard background dim opacity
+        if (!FLAlertLayer::init(75)) return false;
 
+        auto winSize = CCDirector::sharedDirector()->getWinSize();
+        
+        // 1. Create the Background
+        auto bg = CCScale9Sprite::create("GJ_square01.png", {0, 0, 80, 80});
+        bg->setContentSize({320.f, 240.f});
+        bg->setPosition(winSize.width / 2, winSize.height / 2);
+        this->m_mainLayer->addChild(bg);
+        
+        // 2. Create the Title
+        auto title = CCLabelBMFont::create("Macro Settings", "goldFont.fnt");
+        title->setPosition(winSize.width / 2, winSize.height / 2 + 95.f);
+        title->setScale(0.8f);
+        this->m_mainLayer->addChild(title);
+        
+        // 3. Create the Main Buttons
         auto menu = CCMenu::create();
+        menu->setPosition(winSize.width / 2, winSize.height / 2);
         
         auto toggleSpr = ButtonSprite::create("Toggle Macro");
         auto toggleBtn = CCMenuItemSpriteExtra::create(
@@ -21,11 +38,36 @@ protected:
         
         menu->addChild(toggleBtn);
         menu->setLayout(ColumnLayout::create()->setGap(10.f));
-        
-        // m_mainLayer is inherited from the fully defined Popup class now
         this->m_mainLayer->addChild(menu);
 
+        // 4. Create the Close Button
+        auto closeBtnSpr = CCSprite::createWithSpriteFrameName("GJ_closeBtn_001.png");
+        auto closeBtn = CCMenuItemSpriteExtra::create(
+            closeBtnSpr,
+            this,
+            menu_selector(MacroModMenu::onClose)
+        );
+        
+        auto closeMenu = CCMenu::create();
+        closeMenu->setPosition(winSize.width / 2 - 145.f, winSize.height / 2 + 105.f);
+        closeMenu->addChild(closeBtn);
+        this->m_mainLayer->addChild(closeMenu);
+
+        // 5. Enable inputs
+        this->setKeypadEnabled(true);
+        this->setTouchEnabled(true);
+
         return true;
+    }
+
+    void onClose(CCObject*) {
+        this->setKeyboardEnabled(false);
+        this->removeFromParentAndCleanup(true);
+    }
+
+    // Allows the Escape key / Android Back button to close the menu
+    void keyBackClicked() override {
+        this->onClose(nullptr);
     }
 
     void onToggleMacro(CCObject* sender) {
@@ -36,8 +78,7 @@ protected:
 public:
     static MacroModMenu* create() {
         auto ret = new MacroModMenu();
-        // Use init() for Geode v2 compatibility instead of initAnchored()
-        if (ret && ret->init(320.f, 240.f)) { 
+        if (ret && ret->init()) {
             ret->autorelease();
             return ret;
         }
@@ -45,3 +86,4 @@ public:
         return nullptr;
     }
 };
+0
