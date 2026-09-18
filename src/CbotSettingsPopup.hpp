fa3671b555199ceedfdb673310cd55d2723934a0
blazer913 <blazeyah557@gmail.com>
2026-09-18 18:39:46 +0100

    Update CbotSettingsPopup.hpp

--- src/CbotSettingsPopup.hpp
+++ src/CbotSettingsPopup.hpp
@@ -15,26 +15,17 @@ public:
     void setOnCommand(std::function<void(Command)> cb) { m_onCommand = std::move(cb); }
 
 private:
-    void showTab(int index);
     void refreshLabels();
 
-    void onTabMacros(cocos2d::CCObject*);
-    void onTabFrameCounter(cocos2d::CCObject*);
     void onRecord(cocos2d::CCObject*);
     void onPlay(cocos2d::CCObject*);
     void onToggleCounter(cocos2d::CCObject*);
-    void onCalculate(cocos2d::CCObject*);
 
-    cocos2d::CCNode* m_macrosPanel = nullptr;
-    cocos2d::CCNode* m_frameCounterPanel = nullptr;
-
+    cocos2d::CCMenu* m_macrosPanel = nullptr;
     cocos2d::CCLabelBMFont* m_macroStatusLabel = nullptr;
     cocos2d::CCLabelBMFont* m_macroLengthLabel = nullptr;
     CCMenuItemSpriteExtra* m_recordBtn = nullptr;
     CCMenuItemSpriteExtra* m_playBtn = nullptr;
-
-    cocos2d::CCLabelBMFont* m_counterStateLabel = nullptr;
-    cocos2d::CCLabelBMFont* m_calculateResultLabel = nullptr;
 
     std::function<void(Command)> m_onCommand;
 };
