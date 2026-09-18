13e55fccbde400055031ccd342cd14553184a39b
blazer913 <blazeyah557@gmail.com>
2026-09-18 23:05:35 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/PlayLayer.hpp>
-#include <Geode/modify/GJBaseGameLayer.hpp>
+#include <Geode/modify/PlayerObject.hpp>
 #include <Geode/modify/PauseLayer.hpp>
 #include <Geode/ui/Notification.hpp>
 #include "MacroModMenu.hpp"
@@ -8,32 +8,54 @@
 
 using namespace geode::prelude;
 
+class $modify(MyPlayerObject, PlayerObject) {
+    void pushButton(PlayerButton button) {
+        PlayerObject::pushButton(button);
+        this->recordFromPlayerObject(button, true);
+    }
+
+    void releaseButton(PlayerButton button) {
+        PlayerObject::releaseButton(button);
+        this->recordFromPlayerObject(button, false);
+    }
+
+    void recordFromPlayerObject(PlayerButton button, bool isDown) {
+        auto& engine = MacroEngine::get();
+        if (engine.m_state != MacroEngine::State::Recording || engine.m_isPlaybackInput)
+            return;
+
+        auto pl = PlayLayer::get();
+        bool isPlayer1 = !pl || pl->m_player1 == this;
+        engine.recordInput(static_cast<int>(button), isDown, isPlayer1);
+    }
+};
+
 class $modify(MyPlayLayer, PlayLayer) {
     void update(float dt) {
         PlayLayer::update(dt);
-        
+
         auto& engine = MacroEngine::get();
         if (engine.m_state == MacroEngine::State::Idle) return;
 
         engine.m_currentFrame++;
 
         if (engine.m_state == MacroEngine::State::Playing) {
-            while (engine.m_playbackIndex < engine.m_inputs.size() && 
-                   engine.m_inputs[engine.m_playbackIndex].frame <= engine.m_currentFrame) 
+            while (engine.m_playbackIndex < engine.m_inputs.size() &&
+                   engine.m_inputs[engine.m_playbackIndex].frame <= engine.m_currentFrame)
             {
                 auto& input = engine.m_inputs[engine.m_playbackIndex];
-                
-                // Flag to prevent the GJBaseGameLayer hook from recording this artificial click
+
+                // Flag to prevent the PlayerObject hook from re-recording this artificial input
                 engine.m_isPlaybackInput = true;
-                
-                // Route through the layer so game logic (rings, portals) processes it
+
+                // Route through handleButton so game logic (rings, portals) processes it too
                 this->handleButton(input.isDown, input.button, input.isPlayer1);
-                
+
                 engine.m_isPlaybackInput = false;
-                
+
                 engine.m_playbackIndex++;
             }
-            
+
             if (engine.m_playbackIndex >= engine.m_inputs.size()) {
                 engine.m_state = MacroEngine::State::Idle;
                 geode::Notification::create("Playback Finished", NotificationIcon::Success)->show();
@@ -43,31 +65,19 @@ class $modify(MyPlayLayer, PlayLayer) {
 
     void resetLevel() {
         PlayLayer::resetLevel();
-        
+
         auto& engine = MacroEngine::get();
-        
+
         if (this->m_isPracticeMode) {
              engine.syncOnReset();
         } else {
              engine.m_currentFrame = 0;
              engine.m_playbackIndex = 0;
-             
+
              if (engine.m_state == MacroEngine::State::Recording) {
-                 engine.m_inputs.clear(); 
+                 engine.m_inputs.clear();
              }
         }
-    }
-};
-
-class $modify(MyGameLayer, GJBaseGameLayer) {
-    void handleButton(bool isDown, int button, bool isPlayer1) {
-        auto& engine = MacroEngine::get();
-        
-        if (engine.m_state == MacroEngine::State::Recording && !engine.m_isPlaybackInput) {
-            engine.recordInput(button, isDown, isPlayer1);
-        }
-
-        GJBaseGameLayer::handleButton(isDown, button, isPlayer1);
     }
 };
 
@@ -76,7 +86,7 @@ class $modify(MyPauseLayer, PauseLayer) {
         PauseLayer::customSetup();
 
         auto btnSprite = CCSprite::createWithSpriteFrameName("GJ_optionsBtn_001.png");
-        btnSprite->setColor({50, 255, 50}); 
+        btnSprite->setColor({50, 255, 50});
 
         auto modBtn = CCMenuItemSpriteExtra::create(
             btnSprite,
@@ -87,11 +97,11 @@ class $modify(MyPauseLayer, PauseLayer) {
 
         if (auto rightMenu = this->getChildByID("right-button-menu")) {
             rightMenu->addChild(modBtn);
-            rightMenu->updateLayout(); 
-        } 
+            rightMenu->updateLayout();
+        }
     }
 
     void onOpenMacroMenu(CCObject* sender) {
-        MacroModMenu::create(this)->show(); 
+        MacroModMenu::create(this)->show();
     }
 };
