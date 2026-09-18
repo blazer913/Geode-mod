597fbde3424caf526554b7911dc8565c8fe46d3a
blazer913 <blazeyah557@gmail.com>
2026-09-18 12:32:29 +0100

    Update CbotOverlay.hpp

--- src/CbotOverlay.hpp
+++ src/CbotOverlay.hpp
@@ -1,7 +1,5 @@
 #pragma once
 #include <Geode/Geode.hpp>
-#include "Controller.hpp"
-#include <functional>
 #include <string>
 
 namespace cbot {
@@ -13,18 +11,10 @@ public:
 
     void setFrame(int frame);
     void setStatus(std::string const& status);
-    void setCounterVisible(bool visible);
-    void setOnCommand(std::function<void(Command)> cb) { m_onCommand = std::move(cb); }
 
 private:
-    void onRecord(cocos2d::CCObject*);
-    void onPlay(cocos2d::CCObject*);
-    void onToggleCounter(cocos2d::CCObject*);
-
-    cocos2d::CCNode* m_counterNode = nullptr;
     cocos2d::CCLabelBMFont* m_frameLabel = nullptr;
     cocos2d::CCLabelBMFont* m_statusLabel = nullptr;
-    std::function<void(Command)> m_onCommand;
 };
 
 }
